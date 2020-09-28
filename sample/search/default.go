42e674e727c5c1d9308a5031646dd5e90caa5f8c
fangchengjin <fangchengjin@xiaomi.com>
2020-09-28 10:22:41 +0800

    init commit

---
+++ sample/search/default.go
@@ -0,0 +1,14 @@
+package search
+
+type defaultMatcher struct {
+
+}
+
+func init() {
+	var matcher defaultMatcher
+	Register("default", matcher)
+}
+
+func (m defaultMatcher) Search(feed *Feed, searchItem string) ([]*Result, error) {
+	return nil, nil
+}
---
+++ sample/search/search.go
@@ -0,0 +1,51 @@
+package search
+
+import (
+	"log"
+	"sync"
+)
+
+// package level variable
+var matchers = make(map[string]Matcher)
+
+func Run(searchItem string) {
+	// 获取需要搜索的数据源列表
+	feeds, err := RetrieveFeeds()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make(chan *Result)
+
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(len(feeds))
+
+	for _, feed := range feeds {
+		matcher, exists := matchers[feed.Type]
+		if !exists {
+			matcher = matchers["default"]
+		}
+
+		go func(matcher Matcher, feed *Feed) {
+			Match(matcher, feed, searchItem, results)
+			waitGroup.Done()
+		}(matcher, feed)
+	}
+
+	go func() {
+		waitGroup.Wait()
+		close(results)
+	}()
+
+	Display(results)
+}
+
+func Register(feedType string, matcher Matcher) {
+	if _, exists := matchers[feedType]; exists {
+		log.Fatalln(feedType, "Matcher already registered")
+	}
+
+	log.Println("Register", feedType, "matcher")
+	matchers[feedType] = matcher
+}
